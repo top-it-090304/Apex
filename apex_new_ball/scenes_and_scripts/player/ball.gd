cf70a3bc5cecc3844b5dd3e55bb9c161dcd6fb6e
Kontorin <egor.kontorin567@gmail.com>
2026-02-27 23:37:30 +0600

    feat: add rotation and WoodBall skin animated

--- apex_new_ball/scenes_and_scripts/player/ball.gd
+++ apex_new_ball/scenes_and_scripts/player/ball.gd
@@ -2,15 +2,17 @@ extends CharacterBody2D
 
 @export var speed = 300.0
 @export var acceleration = 5000.0 #разгон
-@export var friction = 5000.0 #торможение
+@export var friction = 500.0 #торможение
 @export var jump = -400.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @export var gravity_scale = 1.0 #замедление падения
-@export var gravity_direction = -1 #направление гравитации
+@export var gravity_direction = 1 #направление гравитации
 @export var is_flappy_bird = false
 @export var slide_on_slopes = true
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var alive = true
+var last_position : Vector2
+@export var radius : float = 40 # Радиус мяча в пикселях
 
 func _ready() -> void:
 	velocity = Vector2.ZERO
@@ -20,19 +22,28 @@ func _ready() -> void:
 	floor_max_angle = deg_to_rad(60)
 	floor_constant_speed = false
 	floor_stop_on_slope = not slide_on_slopes
+	last_position = global_position
 	
 func _physics_process(delta: float) -> void:
 	if not alive:
 		return
+		
 	up_direction = Vector2.UP * gravity_direction
+	
+	
+	# Для реализации вращения
+	var distance_moved = global_position.x - last_position.x
+	rotation += distance_moved / radius * gravity_direction
+	last_position = global_position
+	
 	
 	velocity.y += delta * gravity * gravity_scale * gravity_direction
 	
 	var direction = Input.get_axis("move_left", "move_right")
-	if Input.is_action_pressed("move_left"):
-		animated_sprite.flip_h = false
-	if Input.is_action_pressed("move_right"):
-		animated_sprite.flip_h = true
+	#if Input.is_action_pressed("move_left"):
+		#animated_sprite.flip_h = false
+	#if Input.is_action_pressed("move_right"):
+		#animated_sprite.flip_h = true
 	
 	if direction != 0:
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration * delta)
@@ -44,11 +55,6 @@ func _physics_process(delta: float) -> void:
 			velocity.y = jump * gravity_direction
 	elif Input.is_action_just_pressed("move_up"):
 		velocity.y = jump * gravity_direction
-	
-	if gravity_direction == -1:
-		animated_sprite.flip_v = true
-	else:
-		animated_sprite.flip_v = false
 	
 	if slide_on_slopes and is_on_floor():
 		var normal = get_floor_normal()
