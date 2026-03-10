b758d3a9ada88f5dcd4c149456df87f408c5fe6d
Vladimir Pakharenko <vovkavovkamorkovka@mail.ru>
2026-03-10 11:46:33 +0600

    fix parallax

--- apex_new_ball/scenes_and_scripts/ui_and_ux/parallax/parallax.gd
+++ apex_new_ball/scenes_and_scripts/ui_and_ux/parallax/parallax.gd
@@ -2,5 +2,9 @@ extends Parallax2D
 
 var speed = 20 as int
 
-func _process(delta: float) -> void:
-	scroll_offset.x -= speed * delta
+func _ready() -> void:
+	$Surface1.autoscroll = Vector2(-10, 0)
+	$Surface2.autoscroll = Vector2(-12.5, 0)
+	$Surface3.autoscroll = Vector2(-25, 0)
+	$Surface4.autoscroll = Vector2(-45, 0)
+	$Surface5.autoscroll = Vector2(-50, 0)
