93733026527e0462c10e145b3174d91a765fefed
Kontorin <egor.kontorin567@gmail.com>
2026-03-12 20:45:11 +0600

    feat: добавление функции для изменения гравитации на противоположное направление (только для игрока)

--- apex_new_ball/scenes_and_scripts/player/rigid_body_2d.gd
+++ apex_new_ball/scenes_and_scripts/player/rigid_body_2d.gd
@@ -3,32 +3,45 @@ extends RigidBody2D
 @export var move_force = 1500.0
 @export var jump_impulse = 500.0
 @export var max_speed = 400.0
+@export var is_flappy_bird = true
+#var floor_normal = Vector2.UP
+
+#var gravity_inverted = false  # Флаг инвертированной гравитации
 
 var is_on_floor = false
+var is_on_ceiling = false
+var is_on_wall = false
 var floor_normal = Vector2.UP
+var ceiling_normal = Vector2.ZERO
+
 
 func _ready():
-	can_sleep = false  # Запрещаем засыпать
+	can_sleep = false  # Запрещаем засыпать	
 	$AnimatedSprite2D.play()
+	#flip_gravity()   
 
 func _integrate_forces(state):
+	if Input.is_action_just_pressed("flip_gravity"):
+		flip_gravity()
 	move(state)
 
 func check_floor_contact(state):
 	is_on_floor = false
-	floor_normal = Vector2.UP
+
+	var gravity_dir = state.total_gravity.normalized()
+	floor_normal = -gravity_dir 
 	
-	if state.get_contact_count() > 0:
-		for i in range(state.get_contact_count()):
-			var contact_normal = state.get_contact_local_normal(i)
-			if contact_normal.y < -0.5:
-				is_on_floor = true
-				floor_normal = contact_normal
-				break
-				
+	for i in range(state.get_contact_count()):
+		var contact_normal = state.get_contact_local_normal(i)
+		var dot = contact_normal.dot(gravity_dir)
+		if dot < -0.5:
+			is_on_floor = true
+			floor_normal = contact_normal
+			break
+
+
 func move(state):
 	var direction = Input.get_axis("move_left", "move_right")
-	
 	check_floor_contact(state)
 	
 	# Просто применяем силу
@@ -45,3 +58,8 @@ func move(state):
 	if Input.is_action_pressed("move_up") and is_on_floor:
 		apply_central_impulse(Vector2(0, -jump_impulse))
 	
+
+func flip_gravity():
+	gravity_scale = gravity_scale * -1
+	floor_normal = Vector2.DOWN if gravity_scale < 0  else Vector2.UP
+	jump_impulse = jump_impulse * -1
