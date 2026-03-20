347050ce61fed0225732b99d76c47281ae1f4a9e
Vladimir Pakharenko <vovkavovkamorkovka@mail.ru>
2026-03-20 16:30:23 +0600

    меню и кнопки

--- apex_new_ball/scenes_and_scripts/GLOBAL_SCRIPTS/Events.gd
+++ apex_new_ball/scenes_and_scripts/GLOBAL_SCRIPTS/Events.gd
@@ -1,7 +1,13 @@
 extends Node
 
 @warning_ignore("unused_signal")
-signal GAME_ON_PAUSE
+signal SHOW_PAUSE_MODAL
+
+@warning_ignore("unused_signal")
+signal HIDE_PAUSE_MODAL
+
+@warning_ignore("unused_signal")
+signal GAME_ON_LOSE
 
 @warning_ignore("unused_signal")
 signal TOUCHING_THE_FLAG(sprite)
@@ -20,3 +26,6 @@ signal OPEN_THE_DOOR(sprite)
 
 @warning_ignore("unused_signal")
 signal BUTTON_PLAY_PRESSED(button)
+
+@warning_ignore("unused_signal")
+signal PLAYER_RESPAWN
--- apex_new_ball/scenes_and_scripts/GLOBAL_SCRIPTS/Game_manager.gd
+++ apex_new_ball/scenes_and_scripts/GLOBAL_SCRIPTS/Game_manager.gd
@@ -2,36 +2,41 @@ extends Node
 
 var coin = 0
 var flag = 0
+var live = 0
+var resp = 0
+var pause_scene
 
 func _ready() -> void:
-	Events.GAME_ON_PAUSE.connect(_pause)
 	Events.TOUCHING_THE_FLAG.connect(_touch)
 	Events.COLLECTING_COINS.connect(_collect)
 	Events.OPEN_THE_CHEST.connect(_chest)
 	Events.OPEN_THE_DOOR.connect(_door)
 	Events.BUTTON_PLAY_PRESSED.connect(_button)
+	Events.PLAYER_RESPAWN.connect(_respa)
 	
 	for i in range(3):
-		if SaveManager.slot_exists(i):
-			print("существует ", i)
-		else: 
-			print("не существует ", i)
-			var data = SaveManager.get_default_data()
-			data["player"]["id"] = i+1
-			data["player"]["name"] = "Player" + str(i)
-			data["player"]["score"] = 0
-			data["level"]["scene_number"] = "level1"
-			SaveManager.save_slot(i, data)
-
-func _pause():
-	get_tree().paused = true
-	if get_tree().paused:
-		print(52)
+		var loaded = SaveManager.load_slot(i)
+		if loaded["player"]["lives"] < 1:
+			SaveManager.delete_slot(i)
+			print("Сейв слота ", i, " удален (0 жизней)")
+			if SaveManager.slot_exists(i):
+				print("существует ", i)
+			else: 
+				print("не существует ", i)
+				var data = SaveManager.get_default_data()
+				data["player"]["id"] = i+1
+				data["player"]["name"] = "Player" + str(i)
+				data["player"]["score"] = 0
+				SaveManager.save_slot(i, data)
 
 func _touch(sprite):
 	sprite.animation = "flag"
 	flag += 1
 	print("flag")
+
+func _respa():
+	resp += 1
+	print(resp)
 
 func _collect(sprite):
 	sprite.modulate.a = 0
